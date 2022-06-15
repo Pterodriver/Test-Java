7b7c4a5b2ab6805fb9ef1b013e2dcb8685b8fd0d
Vladislav812 <keskeleene@gmail.com>
2022-06-15 20:05:19 +0300

    initial commit of Main.java in Snadbox package

---
+++ Main.java
@@ -0,0 +1,26 @@
+package Sandbox;
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = "";
+        while (scanner.hasNext()) {
+            s = scanner.nextLine();
+            try {
+                int a = Integer.parseInt(s);
+                if (a != 0) {
+                    System.out.println(a * 10);
+                } else {
+                    break;
+                }
+            } catch (NumberFormatException e) {
+                e.getClass().getSimpleName();
+                String[] str = e.getClass().toString().split("\\.");
+                System.out.println(str[str.length -1]);
+            }
+        }
+    }
+}
+
